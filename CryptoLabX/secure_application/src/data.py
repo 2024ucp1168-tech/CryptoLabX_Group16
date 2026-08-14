7ca2d86497ce6e39b6d46857c7853b1b540ae8a4
Garv <2024ucp1153@mnit.ac.in>
2026-08-14 15:39:25 +0530

    Uploading data.py file

---
+++ CryptoLabX/secure_application/src/data.py
@@ -0,0 +1,21 @@
+users = {
+    1: {
+        "username": "garv",
+        "password": "password123",
+        "balance": 1000.0
+    },
+    2: {
+        "username": "alice",
+        "password": "alice123",
+        "balance": 1000.0
+    },
+    3: {
+        "username": "bob",
+        "password": "bob123",
+        "balance": 1000.0
+    }
+}
+
+transactions = []
+
+ADMIN_PASSWORD = "admin123"
